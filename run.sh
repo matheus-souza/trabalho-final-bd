1881617ba97939d5a83c5a8a6f28a138a0357242
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-11-25 02:01:59 -0200

    Ajustado script para inserir dados

--- run.sh
+++ run.sh
@@ -2,36 +2,54 @@
 
 usuario=$1;
 base=$2;
-resetar_base=$3;
+gerar_dados=$3;
+resetar_base=$4;
+
+cria_tabelas_visoes_procedures() {
+  sqls=(
+    'create/create_tables.sql'
+  )
+
+  for sql in ${sqls[@]}; do
+    psql -U${usuario} -d ${base} -a -f ${sql[@]}
+  done
+}
+
+gera_dados_ficticios() {
+  sqls_dados=(
+    'generatedata/tipo_documento.sql'
+  )
+
+  for sql_dado in ${sqls_dados[@]}; do
+    psql -U${usuario} -d ${base} -a -f ${sql_dado[@]}
+  done
+}
 
 if [[ $1 == "--help" || $1 == "-h" ]]
 then
-  echo 'Uso: ./run usuario base [resetar_base]'
-  echo 'Exemplo: ./run postgres reservas true'
+  echo 'Uso: ./run usuario base [gerar_dados] [resetar_base]'
+  echo 'Exemplo: ./run postgres reservas true true'
   exit
 fi
 
 if [[ -z ${usuario} ]]; then
-	echo "Informe o usuário do PostgreSQL";
-	exit 0;
+  echo "Informe o usuário do PostgreSQL";
+  exit 0;
 fi
 
 if [[ -z ${base} ]]; then
-	echo "Informe o nome da base";
-	exit 0;
+  echo "Informe o nome da base";
+  exit 0;
 fi
 
 if [[ $resetar_base == "true" ]]; then
-	echo "Deletando base ${base}"
-	dropdb -U${usuario} ${base};
-	echo "Criando base ${base}"
-	createdb -U${usuario} ${base};
+  echo "Deletando base ${base}"
+  dropdb -U${usuario} ${base};
+  echo "Criando base ${base}"
+  createdb -U${usuario} ${base};
+  cria_tabelas_visoes_procedures
 fi
 
-sqls=(
-	'create/create_tables.sql'
-	)
-
-for sql in ${sqls[@]}; do
-	psql -U${usuario} -d ${base} -a -f ${sql[@]}
-done
+if [[ $gerar_dados == "true" ]]; then
+  gera_dados_ficticios
+fi
