644a4e603b3357323bc4615346928b5df3d0c018
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-11-25 01:13:26 -0200

    Definindo usuário e nome da base por parâmetros

--- run.sh
+++ run.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-dropdb -Uubuntu reservas;
-dropdb -Upostgres reservas;
+usuario=$1;
+base=$2;
+resetar_base=$3;
 
-createdb -Uubuntu reservas;
-createdb -Upostgres reservas;
+if [[ -z ${usuario} ]]; then
+	echo "Informe o usuário do PostgreSQL";
+	exit 0;
+fi
+
+if [[ -z ${base} ]]; then
+	echo "Informe o nome da base";
+	exit 0;
+fi
+
+if [[ $resetar_base == "true" ]]; then
+	echo "Deletando base ${base}"
+	dropdb -U${usuario} ${base};
+	echo "Criando base ${base}"
+	createdb -U${usuario} ${base};
+fi
 
 sqls=(
 	'create/create_tables.sql'
 	)
 
 for sql in ${sqls[@]}; do
-	psql -Uubuntu -d reservas -a -f ${sql[@]}
-	psql -Upostgres -d reservas -a -f ${sql[@]}
+	psql -U${usuario} -d ${base} -a -f ${sql[@]}
+	psql -U${usuario} -d ${base} -a -f ${sql[@]}
 done
