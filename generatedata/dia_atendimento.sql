b6d9f9017ea21b10e3d789046bd1a2536a8d2b82
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-11-28 23:46:38 -0200

    Ajuste na tabela dia_atendimento

--- generatedata/dia_atendimento.sql
+++ generatedata/dia_atendimento.sql
@@ -1,7 +1,7 @@
-INSERT INTO "dia_atendimento" (dia_semana, trabalha_feriado) VALUES
-(1, false),
-(2, true),
-(3, true),
-(4, true),
-(5, false),
-(6, false);
+INSERT INTO "dia_atendimento" (iddia_atendimento, dia_semana, trabalha_feriado) VALUES
+(1, 2, false),
+(2, 3, true),
+(3, 4, true),
+(4, 5, true),
+(5, 6, false),
+(6, 7, false);
