2138c0388acef92980a707e477ec540ee8952172
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-11-29 19:38:51 -0200

    Criado sql para criar tabela com e-mails genéricos

--- run.sh
+++ run.sh
@@ -21,6 +21,7 @@ gera_dados_ficticios() {
     'generatedata/tmp/tmp_documento_valor.sql'
     'generatedata/tmp/tmp_endereco.sql'
     'generatedata/tmp/tmp_nome.sql'
+    'generatedata/tmp/tmp_email.sql'
   )
   for sql_tmp in ${sqls_tmp[@]}; do
     psql -U${usuario} -d ${base} -a -f ${sql_tmp[@]}
