41602890b028bfdf2a9c80ef942f88c9789f5e90
Matheus Henrique de Souza <mh.matheussouza@gmail.com>
2017-12-08 20:09:13 -0200

    Ajuste na view de anos fiscais

--- views/vAnosFiscais.sql
+++ views/vAnosFiscais.sql
@@ -14,13 +14,14 @@ BEGIN
     WHILE v_count <= v_maior_ano
     LOOP
         v_sql := 'CREATE OR REPLACE VIEW ano_fiscal_'||v_count||' as
-        select sum(s.valor) as valor_anual
+        select sum(s.valor) as valor_anual, EXTRACT(YEAR FROM r.data_atendimento) as ano
         from reservas r
         inner join reservas_has_servicos rs
         using (idreservas)
         inner join servicos s
         using (idservicos)
-        where EXTRACT(YEAR FROM r.data_atendimento)::integer =' || v_count;
+        where EXTRACT(YEAR FROM r.data_atendimento)::integer =' || v_count ||
+        'group by ano';
 
         EXECUTE v_sql;
 
